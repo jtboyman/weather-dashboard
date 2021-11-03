185041ca5b277cb5fe30300ad800dae605779139
Edelgard <jtboyman@gmail.com>
2021-11-03 11:36:04 -0400

    display city name and date on search for all cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-
-//fetch the daily weather
-fetch("https://api.openweathermap.org/data/2.5/weather?q=springfield&units=imperial&appid=78a0a45b801ca58af49f2eee9dac4832").then(function(response) {
-       return  response.json()
-}).then(function(data) {
-            console.log(data);
-        });
-//enter text
-//click search
-//get results
-
 let searchCity = function() {
     let cityName = document.getElementById('cityInput').value.trim();
     let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=78a0a45b801ca58af49f2eee9dac4832";
@@ -17,7 +6,7 @@ let searchCity = function() {
         //successful request
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                displayCurrentWeather(data);
             })
         }
 
@@ -28,4 +17,9 @@ let searchCity = function() {
     .catch(function(error) {
         alert("Connection error - please try again later :(");
     })
+};
+
+let displayCurrentWeather = function(data) {
+    let cityDate = document.getElementById("city-date");
+    cityDate.textContent = data.name + "  " + moment().format("MM/DD/YYYY");
 }
