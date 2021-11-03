0f8a6fc6e1aa850b88b0ca85ca1a1297e4e79ee0
Edelgard <jtboyman@gmail.com>
2021-11-03 11:08:23 -0400

    fetches current weather based on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=78a0a45b801ca58af49f2eee9dac4832").then(function(response) {
+
+//fetch the daily weather
+fetch("https://api.openweathermap.org/data/2.5/weather?q=springfield&units=imperial&appid=78a0a45b801ca58af49f2eee9dac4832").then(function(response) {
        return  response.json()
 }).then(function(data) {
             console.log(data);
         });
+//enter text
+//click search
+//get results
+
+let searchCity = function() {
+    let cityName = document.getElementById('cityInput').value.trim();
+    let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=78a0a45b801ca58af49f2eee9dac4832";
+
+    fetch(currentWeatherUrl).then(function(response) {
+        //successful request
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            })
+        }
+
+        else {
+            alert("Oops! We couldn't find that city. Try checking your spelling!");
+        }
+    })
+    .catch(function(error) {
+        alert("Connection error - please try again later :(");
+    })
+}
